3bb724270b0d5fd1a78f5a1ca700a82cdaa294a0
Huy Pham <huyphamthanh8290@gmail.com>
2016-11-13 22:17:15 +0700

    Add Country

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-%w( ).each do |controller|
+%w(  ).each do |controller|
   Rails.application.config.assets.precompile += ["#{controller}.js", "#{controller}.scss"]
 end
